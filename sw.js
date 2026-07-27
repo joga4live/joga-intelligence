8d3947d80061c4c7b6b9eff2a8e5e06ea38475ab
joga4live <omhotien90@gmail.com>
2026-07-27 11:07:05 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    - Estáticos mismo origen y fuentes: stale-while-revalidate (rápido + se actualiza solo).
    Sube CACHE_VERSION cada vez que quieras forzar refresco tras un deploy. */
 
-const CACHE_VERSION = 'joga-v26';
+const CACHE_VERSION = 'joga-v30';
 const CACHE = `joga-cache-${CACHE_VERSION}`;
 
 /* Shell mínimo que se precachea al instalar.
@@ -16,6 +16,8 @@ const CORE = [
   './index.html',
   './retos.html',
   './gate.js',
+  './joga-emblem.svg',
+  './joga-lockup.svg',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png',
